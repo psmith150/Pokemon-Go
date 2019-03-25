e695259e9632d6c122ab192d74d117d07df989e8
psmith150 <patricksmith150@gmail.com>
2019-03-25 00:24:28 -0400

    Added basic UI elements for Pokemon filtering.

--- Pokemon Go Database/Pokemon Go Database/Model/PokemonFilterElement.cs
+++ Pokemon Go Database/Pokemon Go Database/Model/PokemonFilterElement.cs
@@ -55,5 +55,69 @@ namespace Pokemon_Go_Database.Model
             }
         }
         #endregion
+        #region Public Methods
+        public bool EvaluateFilter(Pokemon pokemon)
+        {
+            switch (this.FilterType)
+            {
+                case PokemonFilterType.Name:
+                    switch (this.ComparisonType)
+                    {
+                        case FilterComparisonType.Equal:
+                            return pokemon.Name.Equals(this.FilterValue);
+                        case FilterComparisonType.GreaterThan:
+                            return string.Compare(pokemon.Name, this.FilterValue) >= 1;
+                        case FilterComparisonType.LessThan:
+                            return string.Compare(pokemon.Name, this.FilterValue) <= -1;
+                        case FilterComparisonType.GreaterThanOrEqual:
+                            return string.Compare(pokemon.Name, this.FilterValue) >= 0;
+                        case FilterComparisonType.LessThanOrEqual:
+                            return string.Compare(pokemon.Name, this.FilterValue) <= 0;
+                        case FilterComparisonType.NotEqual:
+                            return !pokemon.Name.Equals(this.FilterValue);
+                        default:
+                            return false;
+                    }
+                case PokemonFilterType.Species:
+                    switch (this.ComparisonType)
+                    {
+                        case FilterComparisonType.Equal:
+                            return pokemon.Species.Species.Equals(this.FilterValue, StringComparison.InvariantCultureIgnoreCase);
+                        case FilterComparisonType.GreaterThan:
+                            return string.Compare(pokemon.Species.Species, this.FilterValue, true) >= 1;
+                        case FilterComparisonType.LessThan:
+                            return string.Compare(pokemon.Species.Species, this.FilterValue, true) <= -1;
+                        case FilterComparisonType.GreaterThanOrEqual:
+                            return string.Compare(pokemon.Species.Species, this.FilterValue, true) >= 0;
+                        case FilterComparisonType.LessThanOrEqual:
+                            return string.Compare(pokemon.Species.Species, this.FilterValue, true) <= 0;
+                        case FilterComparisonType.NotEqual:
+                            return !pokemon.Species.Species.Equals(this.FilterValue, StringComparison.InvariantCultureIgnoreCase);
+                        default:
+                            return false;
+                    }
+                //case PokemonFilterType.Type:
+                //    switch (this.ComparisonType)
+                //    {
+                //        case FilterComparisonType.Equal:
+                //            return pokemon.Species.Type1.ToString().Equals(this.FilterValue, StringComparison.InvariantCultureIgnoreCase) || pokemon.Species.Type2.ToString().Equals(this.FilterValue, StringComparison.InvariantCultureIgnoreCase);
+                //        case FilterComparisonType.GreaterThan:
+                //            return string.Compare(pokemon.Species.Type1.ToString(), this.FilterValue) >= 1;
+                //        case FilterComparisonType.LessThan:
+                //            return string.Compare(pokemon.Species.Species, this.FilterValue) <= -1;
+                //        case FilterComparisonType.GreaterThanOrEqual:
+                //            return string.Compare(pokemon.Species.Species, this.FilterValue) >= 0;
+                //        case FilterComparisonType.LessThanOrEqual:
+                //            return string.Compare(pokemon.Species.Species, this.FilterValue) <= 0;
+                //        case FilterComparisonType.NotEqual:
+                //            return !pokemon.Species.Species.Equals(this.FilterValue);
+                //        default:
+                //            return false;
+                //    }
+                default:
+                    return false;
+            }            
+        }
+        #endregion
     }
 }
--- Pokemon Go Database/Pokemon Go Database/Screens/PokemonViewModel.cs
+++ Pokemon Go Database/Pokemon Go Database/Screens/PokemonViewModel.cs
@@ -26,6 +26,8 @@ namespace Pokemon_Go_Database.Screens
         public ICommand CopyPokemonCommand { get; private set; }
         public ICommand ShowMovesetsCommand { get; private set; }
         public ICommand GoToSpeciesCommand { get; private set; }
+        public ICommand AddFilterCommand { get; private set; }
+        public ICommand RemoveFilterCommand { get; private set; }
         #endregion
 
         private readonly NavigationService navigationService;
@@ -42,9 +44,14 @@ namespace Pokemon_Go_Database.Screens
             this.CopyPokemonCommand = new RelayCommand(async () => await CopyPokemonAsync());
             this.ShowMovesetsCommand = new RelayCommand(async () => await ShowMovesetsAsync());
             this.GoToSpeciesCommand = new RelayCommand(() => GoToSpecies());
+            this.AddFilterCommand = new RelayCommand(() => this.AddFilter());
+            this.RemoveFilterCommand = new RelayCommand<PokemonFilterElement>((filter) => this.RemoveFilter(filter));
 
             this.AllSpecies = this.Session.Pokedex;
             this.MyPokemon = new ListCollectionView(this.Session.MyPokemon);
+            this.Filters = new MyObservableCollection<PokemonFilterElement>();
+            this.Filters.CollectionChanged += (o,a) => this.UpdateFilter();
+            this.Filters.MemberChanged += (o, a) => this.UpdateFilter();
         }
 
         public override void Initialize()
@@ -158,6 +165,18 @@ namespace Pokemon_Go_Database.Screens
                 UpdateFilter();
             }
         }
+        private MyObservableCollection<PokemonFilterElement> _Filters;
+        public MyObservableCollection<PokemonFilterElement> Filters
+        {
+            get
+            {
+                return this._Filters;
+            }
+            private set
+            {
+                this.Set(ref this._Filters, value);
+            }
+        }
         #endregion
 
         #region Private Methods
@@ -175,6 +194,11 @@ namespace Pokemon_Go_Database.Screens
                 result = false;
             if (this.PowerUpFilterActive && !pokemon.ShouldBePoweredUp)
                 result = false;
+            foreach (PokemonFilterElement filter in this.Filters)
+            {
+                if (!filter.EvaluateFilter(pokemon))
+                    result = false;
+            }
             return result;
         }
         private void UpdateFilter()
@@ -230,6 +254,15 @@ namespace Pokemon_Go_Database.Screens
         private void GoToSpecies()
         {
             this.navigationService.NavigateTo<PokedexViewModel>();
+        }
+        private void AddFilter()
+        {
+            this.Filters.Add(new PokemonFilterElement());
+        }
+        private void RemoveFilter(PokemonFilterElement filter)
+        {
+            if (this.Filters != null && this.Filters.Contains(filter))
+                this.Filters.Remove(filter);
         }
         #endregion
     }
